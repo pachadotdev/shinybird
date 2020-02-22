05be9393750ab8e780d479553793c7b17a667f2c
Pachá <mvargas@dcc.uchile.cl>
2020-02-22 18:27:04 -0300

    partial progress with top bar

--- global.R
+++ global.R
@@ -1,97 +1,21 @@
-# shared load ---
 library(shiny)
 
-mbie.cols2 <-
-  ## mbie.cols that supports more than 7 colours
-  function(x = 1:7) {
-    MBIE.cols <- structure(
-      c("#006272", "#97D700", "#00B5E2", "#753BBD", "#DF1995", "#FF6900", "#FBE122"),
-      .Names = c("Teal", "Green", "Blue", "Purple", "Pink", "Orange", "Yellow")
-    )
-    if (x[1] == "Duo1") {
-      x <- 1:2
-    }
-    if (x[1] == "Trio1") {
-      x <- 1:3
-    }
-    if (x[1] == "Duo2") {
-      x <- 2:3
-    }
-    if (x[1] == "Trio2") {
-      x <- 3:5
-    }
-    if (x[1] == "Duo3") {
-      x <- 4:5
-    }
-    if (x[1] == "Trio3") {
-      x <- c(4, 6:7)
-    }
-    if (x[1] == "Duo4") {
-      x <- 6:7
-    }
-    if (x[1] == "Duo5") {
-      x <- c(4, 7)
-    }
-    if (max(x) <= 7 || mode(x) == "character") {
-      as.vector(MBIE.cols[x])
-    } else {
-      colorRampPalette(MBIE.cols)(max(x))[x]
-    }
-  }
-
-# Create a vector of the Ministry of Tourism colours
-Tourism.cols <- c(
-  Forest = "#3D4721FF",
-  Grass = "#A8B50AFF",
-  Southerly = "#5C788FFF",
-  Volcano = "#B09E0DFF",
-  Koura = "#D4470FFF",
-  Merino = "#C2C4A3FF",
-  Moss = "#5E7803FF",
-  Pohutukawa = "#AD2624FF",
-  Sunrise = "#D48500FF",
-  CityLights = "#EDBD3DFF",
-  SouthernCross = "#265787FF",
-  WineCountry = "#61384DFF",
-  Flax = "#708270FF",
-  Ocean = "#A8C4C4FF",
-  RiverStone = "#ADABA6FF",
-  Waka = "#826E59FF",
-  CabbageTree = "#A8AD70FF",
-  Sky = "#94B5E0FF"
-)
-
-# A function tourism.cols() for easy reference to the vector Tourism.cols
-tourism.cols <- function(x = c(1, 2, 3, 5, 12, 6)) {
-  # function to return in vector format a subset of the Ministry of Tourism's 2007
-  # palette of colors.  By default returns 6 colours that form a nice set
-  # for most plots.  Note that normally the 4th colour (Volcano) is too similar
-  # to the 2nd (Grass) for use in plots.
-  if (x[1] == "Primary") x <- 1:6
-  if (x[1] == "Supporting") x <- 7:18
-  if (x[1] == "All") x <- 1:18
-  if (x[1] == "Pale") x <- 13:18
-  if (x[1] == "Alternating") x <- rep(c(1, 6, 4, 8, 2, 16, 3, 5, 15, 18, 9, 14, 7, 12, 11, 17, 10, 13), 6)
-  as.vector(Tourism.cols[x])
+## Creates an appropriately styled title
+tabTitle <- function(x) { 
+  h3(class = "tabTitle", x)
 }
 
-tabTitle <-
-  ## Creates an appropriately styled title
-  function(x)
-    h3(class = "tabTitle", x)
+## Creates an appropriately styled description
+## If NA, returns NULL
+tabDesc <- function(x) { 
+  if (!is.na(x)) tags$p(class = "tabDesc", x) else NULL
+}
 
-tabDesc <-
-  ## Creates an appropriately styled description
-  ## If NA, returns NULL
-  function(x)
-    if (!is.na(x)) tags$p(class = "tabDesc", x) else NULL
-
-tabPwT <-
-  ## tabPanel with Title (using tabTitle)
-  ## Also searches for a match in `tabdesc`
-  ##  (a named vector of descriptions found in "ui_doctabs.R")
-  ## If one is found, adds the description below the title
-  function(title, description, ...) {
+## tabPanel with Title (using tabTitle)
+## Also searches for a match in `tabdesc`
+##  (a named vector of descriptions found in "ui_doctabs.R")
+## If one is found, adds the description below the title
+tabPwT <- function(title, description, ...) {
     tabPanel(
       title,
       div(
@@ -106,8 +30,7 @@ tabPwT <-
 
 ## Modified navbarPage from shiny
 ## Cuts bloat and enables use of tags$head with `thead`
-dashboardPage <-
-  function(title, ..., id = "dashboard", thead = NULL, header = NULL, footer = NULL, windowTitle = title) {
+suitPage <- function(title, ..., id = "dashboard", thead = NULL, header = NULL, footer = NULL, windowTitle = title) {
     pageTitle <- title
     navbarClass <- "navbar navbar-default"
     tabs <- list(...)
@@ -131,23 +54,22 @@ dashboardPage <-
     )
   }
 
-mbie_header <-
-  ## Adds a basic MBIE header, that will only display
-  ##  if the page is not embedded as an iframe.
-  ## Requires: www/mbie-logo.png
-  function() div(
-      id = "mbie-header",
-      div(class = "mbie-topbar"),
+## Adds a basic suit header, that will only display
+##  if the page is not embedded as an iframe.
+suit_header <- function(logo = "logo.svg") {
+  div(
+      id = "suit-header",
       div(
-        class = "mbie-brand",
+        class = "suit-brand",
         tags$a(
-          class = "mbie-brand", href = "http://www.mbie.govt.nz/",
-          title = "Ministry of Business, Innovation & Employment home page.",
+          class = "suit-brand", href = "http://www.duckduckgo.com/",
+          title = "Caption for the logo.",
           tags$img(
-            src = "logo.png",
-            alt = "Ministry of Business, Innovation & Employment"
+            src = logo,
+            alt = "Caption for the logo."
           )
         )
       ),
-      tags$script('if(window != window.parent){$("#mbie-header").css("display", "none");}')
+      tags$script('if(window != window.parent){$("#suit-header").css("display", "none");}')
     )
+}
--- ui.R
+++ ui.R
@@ -1,12 +1,8 @@
 shinyUI(
-  dashboardPage("New Zealand Tourism Dashboard",
+  suitPage("My Organization's Dashboard",
     thead = tagList(
       tags$head(
-        includeCSS("mbie-styles.css"),
-        includeCSS("tdstyles.css"),
-        tags$script(src = "jszip.min.js"),
-        tags$script(src = "accounting.min.js"),
-        tags$script("accounting.settings.currency.precision = 0;"),
+        includeCSS("shinysuit-styles.css"),
         tags$script(src = "jquery-ui-1-11-4.min.js"),
         ## Add jQuery UI tooltips
         ## Use: have class "jui-tip" and
@@ -18,14 +14,12 @@ shinyUI(
             collapsible: true,
             heightStyle: "content"
          });});'),
-        ## Disable DataTable error reporting
-        tags$script('$.fn.dataTableExt.sErrMode = "throw";'),
         ## iframe resizer code to dynamically adjust iframe height
-        ## also requires work by mbie web services to work
+        ## also requires work by suit web services to work
         tags$script(src = "iframeResizer.contentWindow.min.js")
       ),
-      ## Place mbie_header inside a container-fluid for correct positioning
-      div(class = "container-fluid", mbie_header())
+      ## Place suit_header inside a container-fluid for correct positioning
+      div(class = "container-fluid", suit_header())
     ),
     tabPanel(
       "Start",
@@ -34,37 +28,32 @@ shinyUI(
          div(
             class = "front-banner",
             div(class = "imgcon"),
-            div(class = "hcon", h1("The New Zealand"), h1("Tourism Dashboard"))
+            div(class = "hcon", h1("My Organization's"), h1("Dashboard"))
          ),
-         # tags$p(tags$span(class = "warning", "This app is still in active development and has not been officially launched.")),
-         tags$p(class = "intro", "The New Zealand Tourism Dashboard is a one-stop shop for all information about tourism. It brings together a range of tourism datasets produced by MBIE and Statistics New Zealand into one easy-to-use tool. Information is presented using dynamic graphs and data tables."),
+         tags$p(class = "intro", "This is the intro. Much content."),
          div(class = "intro-divider"),
-         tags$p("Main subject-area groupings of tourism data are shown on the toolbar above. To navigate around this site, left-click one of the subject areas and then select one of the related categories in the drop down list."),
-         tags$p(
-            "Graphs can be downloaded as PDFs and tables can be downloaded to a csv file. Information on how to use the graphs and tables, along with an example, is available in the",
-            tags$a("Help", title = "Help Tab", href = "#", id = "HelpTabLink"), "tab.",
-            tags$script("$('#HelpTabLink').click(function(){$('a[data-value=\"Help\"]')[0].click();});")
-         ),
+         tags$p("This is a paragraph."),
+         tags$p("This is another paragraph."),
          div(
             class = "box-con",
             tags$a(
                target = "_blank",
-               href = "http://mbie.govt.nz/info-services/sectors-industries/tourism/tourism-research-data/tourism-data-sources",
+               href = "http://duckduckgo.com",
                div(
                   class = "float box box-more",
                   tags$p(class = "intro", "Find out more"),
-                  tags$p("Click here for more information on the data sources used and their differences.")
+                  tags$p("More content.")
                )
             ),
             tags$a(
                target = "_blank",
-               href = "http://www.mbie.govt.nz/info-services/business/business-growth-agenda/regions",
+               href = "http://duckduckgo.com",
                div(
                   class = "float box box-rear",
                   tags$p(
-                     tags$img(class = "rear-preview", src = "REAR-cover-2015.jpg"),
-                     "The", span(class = "bold", "Regional Economic Activity Report"),
-                     "produced by MBIE presents regional information and trends that supplements the information in this dashboard. Click here to find the online tool and mobile app."
+                    tags$img(class = "rear-preview", src = "REAR-cover-2015.jpg"),
+                    "Here's", span(class = "bold", "much more content"),
+                    "with plenty of detail."
                   )
                )
             )
@@ -80,7 +69,7 @@ shinyUI(
       )
     ),
     navbarMenu(
-      "Overview",
+      "Tab 1",
       tabPwT("Subject 1", "all about subject 1",
              h1("contents"),
              h2("goes here")
@@ -97,6 +86,25 @@ shinyUI(
              h1("contents"),
              h2("goes here")
              )
+    ),
+    navbarMenu(
+      "Tab 2",
+      tabPwT("Subject 1", "all about subject 1",
+             h1("contents"),
+             h2("goes here")
+      ), 
+      tabPwT("Subject 2", "all about subject 2",
+             h1("contents"),
+             h2("goes here")
+      ),
+      tabPwT("Subject 3", "all about subject 3",
+             h1("contents"),
+             h2("goes here")
+      ),
+      tabPwT("Subject 4", "all about subject 4",
+             h1("contents"),
+             h2("goes here")
+      )
     )
   )
 )
