dfd6027a7d27682863166f107bd50477fba2943d
Pachá <mvargas@dcc.uchile.cl>
2020-02-23 16:53:34 -0300

    cleaning css in progress

--- global.R
+++ global.R
@@ -1,28 +1,30 @@
 library(shiny)
 
+color <- "blue"
+
 ## Creates an appropriately styled title
 tabTitle <- function(x) { 
-  h3(class = "tabTitle", x)
+  h1(class = "tabTitle", x)
 }
 
 ## Creates an appropriately styled description
 ## If NA, returns NULL
 tabDesc <- function(x) { 
-  if (!is.na(x)) tags$p(class = "tabDesc", x) else NULL
+  if (!is.na(x)) tags$h2(class = "tabDesc", x) else NULL
 }
 
 ## tabPanel with Title (using tabTitle)
 ## Also searches for a match in `tabdesc`
 ##  (a named vector of descriptions found in "ui_doctabs.R")
 ## If one is found, adds the description below the title
-tabPwT <- function(title, description, ...) {
+tabPanelWithTitle <- function(title, description, ..., color = "red") {
     tabPanel(
       title,
       div(
         class = "tabTitlePanel",
         tabTitle(title),
         tabDesc(description),
-        div(class = "tabTitlePanel-end")
+        div(class = paste0("tabTitlePanel-end-", color))
       ),
       ...
     )
@@ -30,7 +32,7 @@ tabPwT <- function(title, description, ...) {
 
 ## Modified navbarPage from shiny
 ## Cuts bloat and enables use of tags$head with `thead`
-suitPage <- function(title, ..., id = "dashboard", thead = NULL, header = NULL, footer = NULL, windowTitle = title) {
+suitPage <- function(title, ..., id = "dashboard", head = NULL, header = NULL, footer = NULL, windowTitle = title) {
     pageTitle <- title
     navbarClass <- "navbar navbar-default"
     tabs <- list(...)
@@ -48,7 +50,7 @@ suitPage <- function(title, ..., id = "dashboard", thead = NULL, header = NULL, 
       contentDiv <- tagAppendChild(contentDiv, div(class = "row", footer))
     }
     bootstrapPage(
-      title = windowTitle, thead,
+      title = windowTitle, head,
       tags$nav(class = navbarClass, role = "navigation", containerDiv),
       contentDiv
     )
@@ -56,20 +58,91 @@ suitPage <- function(title, ..., id = "dashboard", thead = NULL, header = NULL, 
 
 ## Adds a basic suit header, that will only display
 ##  if the page is not embedded as an iframe.
-suit_header <- function(logo = "logo.svg") {
+suitHeader <- function(logo = NULL, href = NULL, caption = NULL) {
   div(
+    class = "container-fluid",
+    div(
       id = "suit-header",
       div(
         class = "suit-brand",
-        tags$a(
-          class = "suit-brand", href = "http://www.duckduckgo.com/",
-          title = "Caption for the logo.",
-          tags$img(
-            src = logo,
-            alt = "Caption for the logo."
+        if (!is.null(logo)) {
+          tags$a(
+            class = "suit-brand", 
+            href = href,
+            title = caption,
+            tags$img(
+              src = logo,
+              alt = caption
+            )
           )
-        )
+        } else {
+          NULL
+        }
       ),
       tags$script('if(window != window.parent){$("#suit-header").css("display", "none");}')
     )
+  )
+}
+
+frontPage <- function(...) {
+  div(
+    class = "frontp",
+    ...
+  )
+}
+
+frontTitle <- function(title, subtitle = NULL, color = "red") {
+  div(
+    class = "front-banner",
+    div(class = paste0("imgcon-", color)),
+    div(class = "hcon", h1(title)),
+    if (!is.null(subtitle)) div(class = "hcon", h2(subtitle)) else NULL
+  )
+}
+
+introTitle <- function(text, color = "red") {
+  div(
+    tags$p(class = "intro", text),
+    div(class = paste0("intro-divider-", color))
+  )
+}
+
+suitHead <- function(..., color = "red") {
+  color <- switch(
+    color,
+    "red" = "#e44c65", 
+    "green" = "#05878a", 
+    "blue" = "#0074cc"
+  )
+  lst <- rlang::dots_list(
+    tags$head(
+      includeCSS("shinysuit-styles.min.css"),
+      tags$head(tags$style(
+          HTML(sprintf(
+              ".navbar-default .navbar-nav>.active>a, .navbar-default .navbar-nav>.active>a:focus, .navbar-default .navbar-nav>.active>a:hover {
+                background-color: transparent;
+                box-shadow: inset 0 3px 0 0 %s!important
+              }",
+              color
+            ))
+        )),
+      tags$script(src = "jquery-ui-1-11-4.min.js"),
+      ## Add jQuery UI tooltips
+      ## Use: have class "jui-tip" and
+      ##      title attribute = tooltip message
+      ## e.g. tags$div(class = "jui-tip", title = "Tooltip Message", radioButtons(...))
+      tags$script('$(function(){$(".jui-tip").tooltip();});'),
+      ## Use jQuery UI accordion for nice looking show/hide inputs feature
+      tags$script('$(function(){$("div.divinput").accordion({
+            collapsible: true,
+            heightStyle: "content"
+         });});'),
+      ## iframe resizer code to dynamically adjust iframe height
+      ## also requires work by suit web services to work
+      tags$script(src = "iframeResizer.contentWindow.min.js")
+    ),
+    ...
+  )
+  class(lst) <- c("shiny.tag.list", "list")
+  return(lst)
 }
--- ui.R
+++ ui.R
@@ -1,39 +1,23 @@
 shinyUI(
   suitPage("My Organization's Dashboard",
-    thead = tagList(
-      tags$head(
-        includeCSS("shinysuit-styles.min.css"),
-        tags$script(src = "jquery-ui-1-11-4.min.js"),
-        ## Add jQuery UI tooltips
-        ## Use: have class "jui-tip" and
-        ##      title attribute = tooltip message
-        ## e.g. tags$div(class = "jui-tip", title = "Tooltip Message", radioButtons(...))
-        tags$script('$(function(){$(".jui-tip").tooltip();});'),
-        ## Use jQuery UI accordion for nice looking show/hide inputs feature
-        tags$script('$(function(){$("div.divinput").accordion({
-            collapsible: true,
-            heightStyle: "content"
-         });});'),
-        ## iframe resizer code to dynamically adjust iframe height
-        ## also requires work by suit web services to work
-        tags$script(src = "iframeResizer.contentWindow.min.js")
-      ),
+    head = suitHead(
       ## Place suit_header inside a container-fluid for correct positioning
-      div(class = "container-fluid", suit_header())
+      suitHeader(logo = "logo.svg",
+                 href = "http://www.duckduckgo.com/",
+                 caption = "Caption for the logo."),
+      color = "blue"
     ),
+    
     tabPanel(
       "Start",
-      div(
-         class = "frontp",
-         div(
-            class = "front-banner",
-            div(class = "imgcon"),
-            div(class = "hcon", h1("My Organization's"), h1("Dashboard"))
-         ),
-         tags$p(class = "intro", "This is the intro. Much content."),
-         div(class = "intro-divider"),
+      frontPage(
+         frontTitle("My Organization's Dashboard", "Much content, very nice", color = "blue"),
+         
+         introTitle("Foo Bar", color = "blue"),
+         
          tags$p("This is a paragraph."),
          tags$p("This is another paragraph."),
+         
          div(
             class = "box-con",
             tags$a(
@@ -58,6 +42,7 @@ shinyUI(
                )
             )
          ),
+         
          div(
             class = "box box-timeout",
             tags$p(
@@ -68,40 +53,42 @@ shinyUI(
          )
       )
     ),
+    
     navbarMenu(
       "Tab 1",
-      tabPwT("Subject 1", "all about subject 1",
+      tabPanelWithTitle("Subject 1", "all about subject 1", color = "red",
              h1("contents"),
              h2("goes here")
              ), 
-      tabPwT("Subject 2", "all about subject 2",
+      tabPanelWithTitle("Subject 2", "all about subject 2", color = "red",
              h1("contents"),
              h2("goes here")
              ),
-      tabPwT("Subject 3", "all about subject 3",
+      tabPanelWithTitle("Subject 3", "all about subject 3", color = "red",
              h1("contents"),
              h2("goes here")
              ),
-      tabPwT("Subject 4", "all about subject 4",
+      tabPanelWithTitle("Subject 4", "all about subject 4", color = "red",
              h1("contents"),
              h2("goes here")
              )
     ),
+    
     navbarMenu(
       "Tab 2",
-      tabPwT("Subject 1", "all about subject 1",
+      tabPanelWithTitle("Subject 1", "all about subject 1", color = "red",
              h1("contents"),
              h2("goes here")
       ), 
-      tabPwT("Subject 2", "all about subject 2",
+      tabPanelWithTitle("Subject 2", "all about subject 2", color = "red",
              h1("contents"),
              h2("goes here")
       ),
-      tabPwT("Subject 3", "all about subject 3",
+      tabPanelWithTitle("Subject 3", "all about subject 3", color = "red",
              h1("contents"),
              h2("goes here")
       ),
-      tabPwT("Subject 4", "all about subject 4",
+      tabPanelWithTitle("Subject 4", "all about subject 4", color = "red",
              h1("contents"),
              h2("goes here")
       )
